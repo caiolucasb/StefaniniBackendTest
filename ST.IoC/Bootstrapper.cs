88c4e99d4c288ecf6dc12273d22f1c74067f97ab
caiolucasb <caio.maia@marttech.com.br>
2022-07-26 00:06:55 -0300

    Create IoC and configure dependency injection

---
+++ ST.IoC/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using ST.Repository.Context;
+using ST.Repository.Repositories;
+using ST.CrossCutting.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ST.IoC
+{
+    public static class Bootstrapper
+    {
+        public static void ConfigureStApp(this IServiceCollection services, IConfiguration configuration)
+        {
+            ConfigureRepositories(services, configuration);
+        }
+        private static void ConfigureRepositories(IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PersonContext>(options =>
+             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IPersonRepository, PersonRepository>();
+        }
+    }
+}
