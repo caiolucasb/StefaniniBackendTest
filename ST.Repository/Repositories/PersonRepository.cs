a3cc42a8c6e57bd7330a3e0e30fb666b0d7eed13
caiolucasb <caio.maia@marttech.com.br>
2022-07-26 05:18:40 -0300

    Fix wrong methods in repository

--- ST.Repository/Repositories/PersonRepository.cs
+++ ST.Repository/Repositories/PersonRepository.cs
@@ -13,45 +13,59 @@ namespace ST.Repository.Repositories
         {
             _personContext = personContext;
         }
-        public async Task PostPerson(PersonTO person)
+        public async Task CreatePerson(PersonTO person)
         {
-            var personData = new Person(person.Name, person.CPF, person.Age, person.CityId);
+            var personData = new Person(person.Name, person.Age, person.CPF, person.CityId);
             await _personContext.AddAsync(personData);
             await _personContext.SaveChangesAsync();
         }
-        public async Task<PersonTO> GetPerson(int id)
+        public async Task<PersonTO> ReadPerson(int id)
         {
             var personData = await _personContext.People.Where(x => x.Id == id).FirstOrDefaultAsync() ?? new Person();
             var personTO = new PersonTO(personData.Id,  personData.Name, personData.Age, personData.CPF, personData.CityId);
             return personTO;
         }
-        public async Task UpdatePerson(PersonTO person)
+        public async Task UpdatePerson(int id,PersonTO person)
         {
-            var personData = new Person(person.Name, person.CPF, person.Age, person.CityId);
-            _personContext.People.Update(personData);
+            var personData = await _personContext.People.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if(personData != null)
+            {
+                personData.Name = person.Name;
+                personData.Age = person.Age;
+                personData.CPF = person.CPF;
+                personData.CityId = person.CityId;
+
+                _personContext.People.Update(personData);
+            }
             await _personContext.SaveChangesAsync();
         }
         public async Task DeletePerson(int id)
         {
             var personData = await _personContext.People.Where(x => x.Id == id).FirstOrDefaultAsync();
             _personContext.People.Remove(personData);
+            await _personContext.SaveChangesAsync();
         }
-        public async Task<CityTO> GetCity(int id)
+        public async Task<CityTO> ReadCity(int id)
         {
             var cityData = await _personContext.Cities.Where(x => x.Id == id).FirstOrDefaultAsync() ?? new City();
             var cityTO = new CityTO(cityData.Id, cityData.Name, cityData.UF);
             return cityTO;
         }
-        public async Task PostCity(CityTO city)
+        public async Task CreateCity(CityTO city)
         {
             var cityData = new City(city.Name, city.UF);
             await _personContext.AddAsync(cityData);
             await _personContext.SaveChangesAsync();
         }
-        public async Task UpdateCity(CityTO city)
+        public async Task UpdateCity(int id,CityTO city)
         {
-            var cityData = new City(city.Name, city.UF);
-            _personContext.Cities.Update(cityData);
+            var cityData = await _personContext.Cities.Where(x => x.Id == id).FirstOrDefaultAsync();
+            if (cityData != null)
+            {
+                cityData.Name = city.Name;
+                cityData.UF = city.UF;
+                _personContext.Cities.Update(cityData);
+            }
             await _personContext.SaveChangesAsync();
         }
     }
