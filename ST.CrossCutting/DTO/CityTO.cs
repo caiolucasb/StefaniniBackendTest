c355cde2b63bb001b28dd7611435d20b2a783045
caiolucasb <caio.maia@marttech.com.br>
2022-07-26 00:08:27 -0300

    Create DTOs

---
+++ ST.CrossCutting/DTO/CityTO.cs
@@ -0,0 +1,18 @@
+﻿namespace ST.CrossCutting.DTO
+{
+    public class CityTO
+    {
+        public CityTO() { }
+
+        public CityTO(int id, string name, string uf)
+        {
+            Id = id;
+            Name = name;
+            UF = uf;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string UF { get; set; } = null!;
+    }
+}
