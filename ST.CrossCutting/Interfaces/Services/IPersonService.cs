59a922d2883d7a18a3f6da0869b2a64c5d321b00
caiolucasb <caio.maia@marttech.com.br>
2022-07-26 19:37:36 -0300

    Add the service interface to the crosscutting

---
+++ ST.CrossCutting/Interfaces/Services/IPersonService.cs
@@ -0,0 +1,16 @@
+﻿using ST.CrossCutting.DTO;
+using ST.CrossCutting.Models;
+
+namespace ST.CrossCutting.Interfaces.Services
+{
+    public interface IPersonService
+    {
+        Task<ReturnObject<PersonTO>> GetPerson(int id);
+        Task<ReturnObject<PersonTO>> PostPerson(PersonTO person);
+        Task<ReturnObject<PersonTO>> PutPerson(int id, PersonTO person);
+        Task<ReturnObject<PersonTO>> DeletePerson(int id);
+        Task<ReturnObject<CityTO>> GetCity(int id);
+        Task<ReturnObject<CityTO>> PostCity(CityTO city);
+        Task<ReturnObject<CityTO>> PutCity(int id, CityTO city);
+    }
+}
